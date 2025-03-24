4d2b5b66d7c129f1ca4728501ec6ce7dde7160ab
iamjaeeuncho <redjoun@gmail.com>
2025-03-24 19:03:26 +0900

    Add logos

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -2,7 +2,20 @@ import React from "react";
 
 const Footer = () => (
   <footer className="footer">
-    © 2025 iamjaeeuncho. All rights reserved.
+    <div className="logos">
+      <a href="https://github.com/iamjaeeuncho" target="_blank" rel="noopener noreferrer">
+        <img className="logo" src="/logos/github.png" alt="GitHub Logo" />
+      </a>
+      <a href="https://www.linkedin.com/in/jaeeuncho" target="_blank" rel="noopener noreferrer">
+        <img className="logo" src="/logos/linkedin.png" alt="Linkedin Logo" />
+      </a>
+      <a href="https://x.com/iamjaeeuncho" target="_blank" rel="noopener noreferrer">
+        <img className="logo" src="/logos/twitter.png" alt="Twitter Logo" />
+      </a>
+    </div>
+    <div className="copyright">
+      © 2025 iamjaeeuncho. All rights reserved.
+    </div>
   </footer>
 );
 
