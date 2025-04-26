c080f1c5944660d50abd8ee1f35807dfe4a43c4c
iamjaeeuncho <redjoun@gmail.com>
2025-04-26 17:10:25 +0900

    Add notfound page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Project from './pages/Project';
-import Essay from './pages/Essay';
-import Article from './pages/Article';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Essay from "./pages/Essay";
+import Article from "./pages/Article";
+import NotFound from "./pages/NotFound";
+
+import "./App.css";
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
             <Route path="/project" element={<Project />} />
             <Route path="/essay" element={<Essay />} />
             <Route path="/article/:id" element={<Article />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
         <Footer />
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <p className="notfound-message">페이지를 찾을 수 없습니다.</p>
+    </div>
+  );
+};
+
+export default NotFound;
